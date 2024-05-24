89f061400267cdc782803ffe615316b46faeacf3
Martin Klingenberg <martin.klingenberg@alv.no>
2024-05-24 11:01:50 +0200

    Made it possible to create a project setup and update package versions

---
+++ config-files.js
@@ -0,0 +1,153 @@
+
+export const WEBPACK_DEV = `
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: [path.join(__dirname, "demo"), path.join(__dirname, "dist")],
+    compress: true,
+    port: 8080,
+    historyApiFallback: true, // Handy for developing SPA's
+  },
+});
+`;
+
+export const WEBPACK_PROD = `
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+  mode: "production",
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /^\.\/src\/config\/config.ts/, // src/main.ts
+      "./src/config/config.prod.ts",
+    ),
+  ],
+});
+`;
+
+export const WEBPACK_COMMON = `
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+  },
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.css|\.s(c|a)ss$/,
+        use: ["lit-scss-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunksSortMode: "none",
+      template: "src/index.html",
+    }),
+  ],
+};
+
+`;
+
+export const ESLINT_CONFIG_FILE = `
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];`;
+
+
+export const KARMA_CONFIG_FILE = `
+module.exports = (config) => {
+  config.set({
+    // ... normal karma configuration
+
+    // make sure to include webpack as a framework
+    frameworks: ["jasmine", "webpack"],
+    reporters: ['mocha'],
+    plugins: ["karma-webpack", "karma-jasmine", "karma-mocha-reporter", "karma-chrome-launcher"],
+
+    files: [
+      { pattern: "src/**/*.spec.ts", watched: false },
+      //{ pattern: "src/**/*.test.ts", watched: false },
+    ],
+    browsers: ["ChromeHeadless"],
+    preprocessors: {
+      // add webpack as preprocessor
+      "src/**/*.spec.ts": ["webpack" ],
+      "src/**/*.test.ts": ["webpack" ],
+    },
+    logLevel: config.LOG_WARN,
+    webpack: {
+      stats: 'errors-only',
+      devtool: 'inline-source-map',
+      resolve: {
+        extensions: [".ts", ".js"],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts?$/,
+            use: "ts-loader",
+            exclude: [/node_modules/],
+          },
+          {
+            test: /\.css|\.s(c|a)ss$/,
+            use: ["lit-scss-loader", "sass-loader"],
+          },
+        ],
+      },
+    },
+  });
+};`;
+
+export const TSCONFIG = `
+{
+  "compilerOptions": {
+    "target": "es2020",
+    "module": "es2020",
+    "sourceMap": true,
+    "strict": true,
+    "moduleResolution": "node",
+    "rootDirs": [
+      "src"
+    ],
+    "types": [
+      "./node_modules/lit-scss-loader/types.d.ts",
+      "jasmine"
+    ],
+    "esModuleInterop": true,
+    "experimentalDecorators": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true,
+  },
+  "files": ["./src/index.ts"],
+  "include": ["./node_modules/lit-scss-loader/types.d.ts"]
+}`;
--- project-initialiser.js
+++ project-initialiser.js
@@ -1,66 +1,8 @@
-import { getProjectRoot } from "./config-reader"
+import { getProjectRoot } from "./config-reader.js"
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { join } from 'path';
-
-/**
- * @typedef PackageManifest
- * @property { Record<string, string> } dependencies
- * @property { Record<string, string> } devDependencies
- * @property { Record<string, string> } scripts
- * */
-
-export const createOrUpdateProjectPackages = () => {
-  console.log(chalk.blue('UPDATING DEPS  ') + chalk.gray('Should be done in a few seconds'));
-  modifyPackageJson();
-  createFiles();
-  console.log(chalk.blue('COMPLETED ') + chalk.gray('Run yarn or npm i to complete update'));
-}
-
-const modifyPackageJson = () => {
-  console.log(chalk.green('UPDATING  ') + chalk.gray('package.json'))
-  const projectRoot = getProjectRoot();
-  const packageFilePath = join(projectRoot, 'package.json');
-
-  const packageFile =  readFileSync(packageFilePath, 'utf-8');
-
-  /** @type {PackageManifest} */
-  const packageFileContents = JSON.parse(packageFile);
-
-  DEV_DEPS.forEach(dependency => {
-      packageFileContents.devDependencies[dependency[0]] = dependency[1];
-  });
-
-  DEPS.forEach(dependency => {
-      packageFileContents.dependencies[dependency[0]] = dependency[1];
-  });
-
-  SCRIPTS.forEach(script => {
-    packageFileContents.scripts[script[0]] = script[1];
-  });
-
-  writeFileSync()
-}
-
-const createFiles = () => {
-  const projectRoot = getProjectRoot();
-
-  console.log(chalk.green('CREATING PROJECT CONFIGURATINO FILES'));
-
-  CONFIG_FILES.forEach(configFile => {
-    console.log(chalk.green('CREATING  ') + chalk.gray(configFile.name));
-    writeFileSync(join(projectRoot, configFile.name), configFile.content);
-  });
-
-  NON_OVERWRITE_CONFIG_FILES.forEach(webpackConfiguration => {
-    const filename = join(projectRoot, webpackConfiguration.name);
-    if (existsSync(filename)) {
-      return;
-    }
-
-    console.log(chalk.green('CREATING  ') + chalk.gray(webpackConfiguration.name));
-    writeFileSync(filename, webpackConfiguration.content);
-  });
-}
+import { ESLINT_CONFIG_FILE, KARMA_CONFIG_FILE, TSCONFIG, WEBPACK_COMMON, WEBPACK_DEV, WEBPACK_PROD } from "./config-files.js";
+import chalk from 'chalk';
 
 const CONFIG_FILES = [
   {name: 'eslint.config.mjs', content: ESLINT_CONFIG_FILE},
@@ -73,159 +15,6 @@ const NON_OVERWRITE_CONFIG_FILES = [
   { name: 'webpack.dev.js', content: WEBPACK_DEV },
   { name: 'tsconfig.json', content: TSCONFIG }
 ];
-
-const WEBPACK_DEV = `
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const path = require("path");
-
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    static: [path.join(__dirname, "demo"), path.join(__dirname, "dist")],
-    compress: true,
-    port: 8080,
-    historyApiFallback: true, // Handy for developing SPA's
-  },
-});
-`;
-
-const WEBPACK_PROD = `
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const webpack = require("webpack");
-
-module.exports = merge(common, {
-  mode: "production",
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(
-      /^\.\/src\/config\/config.ts/, // src/main.ts
-      "./src/config/config.prod.ts",
-    ),
-  ],
-});
-`;
-
-const WEBPACK_COMMON = `
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-module.exports = {
-  devServer: {
-    historyApiFallback: true,
-  },
-  mode: "development",
-  entry: "./src/index.ts",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: "ts-loader",
-        exclude: [/node_modules/],
-      },
-      {
-        test: /\.css|\.s(c|a)ss$/,
-        use: ["lit-scss-loader", "sass-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      chunksSortMode: "none",
-      template: "src/index.html",
-    }),
-  ],
-};
-
-`;
-
-const ESLINT_CONFIG_FILE = `
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];`;
-
-
-const KARMA_CONFIG_FILE = `
-module.exports = (config) => {
-  config.set({
-    // ... normal karma configuration
-
-    // make sure to include webpack as a framework
-    frameworks: ["jasmine", "webpack"],
-    reporters: ['mocha'],
-    plugins: ["karma-webpack", "karma-jasmine", "karma-mocha-reporter", "karma-chrome-launcher"],
-
-    files: [
-      { pattern: "src/**/*.spec.ts", watched: false },
-      //{ pattern: "src/**/*.test.ts", watched: false },
-    ],
-    browsers: ["ChromeHeadless"],
-    preprocessors: {
-      // add webpack as preprocessor
-      "src/**/*.spec.ts": ["webpack" ],
-      "src/**/*.test.ts": ["webpack" ],
-    },
-    logLevel: config.LOG_WARN,
-    webpack: {
-      stats: 'errors-only',
-      devtool: 'inline-source-map',
-      resolve: {
-        extensions: [".ts", ".js"],
-      },
-      module: {
-        rules: [
-          {
-            test: /\.ts?$/,
-            use: "ts-loader",
-            exclude: [/node_modules/],
-          },
-          {
-            test: /\.css|\.s(c|a)ss$/,
-            use: ["lit-scss-loader", "sass-loader"],
-          },
-        ],
-      },
-    },
-  });
-};`;
-
-const TSCONFIG = `
-{
-  "compilerOptions": {
-    "target": "es2020",
-    "module": "es2020",
-    "sourceMap": true,
-    "strict": true,
-    "moduleResolution": "node",
-    "rootDirs": [
-      "src"
-    ],
-    "types": [
-      "./node_modules/lit-scss-loader/types.d.ts",
-      "jasmine"
-    ],
-    "esModuleInterop": true,
-    "experimentalDecorators": true,
-    "skipLibCheck": true,
-    "forceConsistentCasingInFileNames": true,
-  },
-  "files": ["./src/index.ts"],
-  "include": ["./node_modules/lit-scss-loader/types.d.ts"]
-}`;
 
 
 const SCRIPTS = [
@@ -272,3 +61,74 @@ const DEPS = [
     ["rxjs", "^7.8.1"],
     ["@klingen/litworks", "^1.1.3"]
 ];
+
+/**
+ * @typedef PackageManifest
+ * @property { Record<string, string> | undefined } dependencies
+ * @property { Record<string, string> | undefined } devDependencies
+ * @property { Record<string, string> } scripts
+ * */
+
+export const createOrUpdateProjectPackages = () => {
+  console.log(chalk.blue('UPDATING DEPS  ') + chalk.gray('Should be done in a few seconds'));
+  modifyPackageJson();
+  createFiles();
+  console.log(chalk.blue('COMPLETED ') + chalk.gray('Run yarn or npm i to complete update'));
+}
+
+const modifyPackageJson = () => {
+  console.log(chalk.green('UPDATING  ') + chalk.gray('package.json'))
+  const projectRoot = getProjectRoot();
+  const packageFilePath = join(projectRoot, 'package.json');
+
+  const packageFile =  readFileSync(packageFilePath, 'utf-8');
+
+  /** @type {PackageManifest} */
+  const packageFileContents = JSON.parse(packageFile);
+
+  if (!packageFileContents.dependencies) {
+    packageFileContents.dependencies = {};
+  }
+
+  if (!packageFileContents.devDependencies) {
+    packageFileContents.devDependencies = {};
+  }
+
+  DEV_DEPS.forEach(dependency => {
+    packageFileContents.devDependencies[dependency[0]] = dependency[1];
+  });
+
+  DEPS.forEach(dependency => {
+    packageFileContents.dependencies[dependency[0]] = dependency[1];
+  });
+
+  SCRIPTS.forEach(script => {
+    packageFileContents.scripts[script[0]] = script[1];
+  });
+
+  writeFileSync(packageFilePath, JSON.stringify(packageFileContents, null, 2));
+}
+
+const createFiles = () => {
+  const projectRoot = getProjectRoot();
+
+  console.log(chalk.green('CREATING PROJECT CONFIGURATINO FILES'));
+
+  CONFIG_FILES.forEach(configFile => {
+    console.log(chalk.green('CREATING  ') + chalk.gray(configFile.name));
+    writeFileSync(join(projectRoot, configFile.name), configFile.content);
+  });
+
+  NON_OVERWRITE_CONFIG_FILES.forEach(webpackConfiguration => {
+    const filename = join(projectRoot, webpackConfiguration.name);
+    if (existsSync(filename)) {
+      return;
+    }
+
+    console.log(chalk.green('CREATING  ') + chalk.gray(webpackConfiguration.name));
+    writeFileSync(filename, webpackConfiguration.content);
+  });
+}
+
+
+
